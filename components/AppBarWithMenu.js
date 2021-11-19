eb2418186655a49a9aa14dd51c6e4f20464f9054
pheno <pheno_the_best@yahoo.com.tw>
2021-11-19 20:25:01 +0800

    app bar first version

--- components/AppBarWithMenu.js
+++ components/AppBarWithMenu.js
@@ -1,10 +1,32 @@
 import React from "react";
-import { Box, AppBar } from "@mui/material";
+import { Box, AppBar, Toolbar, IconButton, Typography } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import AccountCircle from "@mui/icons-material/AccountCircle";
 
 export default function AppBarWithMenu() {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">Pheno 書店</AppBar>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="選單"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Pheno 書店
+          </Typography>
+          <div>
+            <IconButton>
+              <AccountCircle />
+            </IconButton>
+          </div>
+        </Toolbar>
+      </AppBar>
     </Box>
   );
 }
