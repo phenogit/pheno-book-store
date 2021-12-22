c2482a6224ca67efdc42d994d132b937da81d25a
pheno <pheno_the_best@yahoo.com.tw>
2021-12-22 20:28:13 +0800

    add app bar to book page

--- pages/book/[title].js
+++ pages/book/[title].js
@@ -1,3 +1,19 @@
+import Head from "next/head";
+import { useSession } from "next-auth/client";
+import { useRouter } from "next/router";
+
+import AppBarWithMenu from "../../components/AppBarWithMenu";
+
 export default function Book() {
-  return <>書本介紹頁</>;
+  const [session] = useSession();
+  return (
+    <>
+      <Head>
+        <title>介紹頁</title>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <AppBarWithMenu session={session} />
+      書本介紹
+    </>
+  );
 }
